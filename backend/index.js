dee144f5f656adf7d9bc3f823e0307f6783a9930
Dhruv_D <dogradhruv2005@gmail.com>
2025-03-22 16:57:32 +0530

    connection_established

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
        useNewUrlParser: true,
-        useUnifiedTopology: true,
+       useUnifiedTopology: true,
     });
     console.log("Mongoose connected");
   } catch (error) {
